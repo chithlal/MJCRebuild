f485af622d3a3b012971ec827b81f6a09f40e1b9
dword0 <dword0@gmail.com>
2020-12-15 20:26:52 +0530

    1st commit after singup

--- app/src/main/java/com/developer/mjc/signup/SignUpViewModel.kt
+++ app/src/main/java/com/developer/mjc/signup/SignUpViewModel.kt
@@ -1,8 +1,15 @@
 package com.developer.mjc.signup
 
+import android.app.Activity
+import android.content.Context
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.developer.mjc.firebase.FirestoreClass
 import com.developer.mjc.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.ktx.Firebase
 
 
 class SignUpViewModel(): ViewModel() {
@@ -10,9 +17,18 @@ class SignUpViewModel(): ViewModel() {
      val signLiveData = MutableLiveData<User>()
 
 
-    fun signUp(signupData: User){
-        if(suucess){
-            signLiveData.postValue("")
+    fun signUp(signupData: User, pwd:String,context:Context){
+// Need to replace "Abcd@1234" with the password from user
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(signupData.email,pwd).addOnCompleteListener {
+            task ->
+            if(task.isSuccessful){
+                val firebaseUser : FirebaseUser = task.result!!.user!!
+                val user = User(firebaseUser.uid,signupData.name,signupData.email,signupData.phone)
+                FirestoreClass().registerUser(context as Activity,user)
+                signupData.id = firebaseUser.uid
+                signLiveData.postValue(signupData)
+            }
         }
+
     }
 }
